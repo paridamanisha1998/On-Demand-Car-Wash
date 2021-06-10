a547dd391fff153c63ed48d6d83e39a5b13eec58
paridamanisha1998 <63549388+paridamanisha1998@users.noreply.github.com>
2021-06-10 11:03:10 +0530

    Discover Server Added

---
+++ Discover_Server/src/test/java/io/cloud/service/Discover_Server/DiscoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cloud.service.Discover_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
