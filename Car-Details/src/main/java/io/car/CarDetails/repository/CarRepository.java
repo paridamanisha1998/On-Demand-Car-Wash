8d02c5820ae2cce98e26558d62c0a3bb089b0e74
paridamanisha1998 <63549388+paridamanisha1998@users.noreply.github.com>
2021-06-10 15:42:51 +0530

    Delete Car-Details directory

--- Car-Details/src/main/java/io/car/CarDetails/repository/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package io.car.CarDetails.repository;
-
-import io.car.CarDetails.model.*;
-import org.springframework.data.mongodb.repository.*;
-
-public interface CarRepository extends MongoRepository<Car,String> {
-
-}
