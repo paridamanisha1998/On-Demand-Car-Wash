8b46f5ce682ac1ba00bf286641626fe47f83f477
paridamanisha1998 <63549388+paridamanisha1998@users.noreply.github.com>
2021-06-12 22:08:20 +0530

    Swagger Added

--- User_Application/src/main/java/com/casestudy/User_Application/model/Users.java
+++ User_Application/src/main/java/com/casestudy/User_Application/model/Users.java
@@ -1,25 +1,32 @@
 package com.casestudy.User_Application.model;
+import lombok.*;
+import org.springframework.data.annotation.*;
 import org.springframework.data.mongodb.core.mapping.*;
-import java.lang.reflect.*;
+import org.springframework.stereotype.*;
+
+import java.util.*;
 
 @Document(value = "userinfo")
 public class Users {
+    @Id
     private String userId;
     private String userName;
     private String userContact;
     private String userEmail;
-    private String userAddress;
+    private Address address;
     private String password;
-    private CarDetails carDetails;
-    public Users(String userId, String userName, String userContact, String userEmail, String userAddress, String password) {
+    private Car car;
+    public Users(){
+
+    }
+    public Users(String userId, String userName, String userContact, String userEmail, Address address, String password,Car car) {
         this.userId = userId;
         this.userName = userName;
         this.userContact = userContact;
         this.userEmail = userEmail;
-        this.userAddress = userAddress;
+        this.address = address;
         this.password = password;
-    }
-    public Users() {
+        this.car=car;
     }
 
     public String getUserId() {
@@ -54,12 +61,12 @@ public class Users {
         this.userEmail = userEmail;
     }
 
-    public String getUserAddress() {
-        return userAddress;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setUserAddress(String userAddress) {
-        this.userAddress = userAddress;
+    public void setAddress(Address address) {
+        this.address = address;
     }
 
     public String getPassword() {
@@ -68,5 +75,13 @@ public class Users {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
     }
 }
