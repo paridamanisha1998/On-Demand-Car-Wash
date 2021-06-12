f954f2b50eb9cd6e2d9e6e07d353dfae687c8d64
paridamanisha1998 <63549388+paridamanisha1998@users.noreply.github.com>
2021-06-12 22:06:39 +0530

    Delete User_Application/User_Application directory

--- User_Application/User_Application/src/main/java/com/casestudy/User_Application/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.casestudy.User_Application.repository;
-
-import com.casestudy.User_Application.model.*;
-import org.springframework.data.mongodb.repository.*;
-
-import java.util.*;
-public interface UserRepository extends MongoRepository<Users,String> {
-}
