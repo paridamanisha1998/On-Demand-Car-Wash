5a247d2e75b88cdc397bbdee762f9de926bc5baa
paridamanisha1998 <63549388+paridamanisha1998@users.noreply.github.com>
2021-06-12 22:03:58 +0530

    Functionality, Eureka client ,Swagger Added

--- Admin_Application/src/main/java/io/adminapplication/Admin_Application/AdminApplication.java
+++ Admin_Application/src/main/java/io/adminapplication/Admin_Application/AdminApplication.java
@@ -2,12 +2,21 @@ package io.adminapplication.Admin_Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.*;
+import org.springframework.cloud.netflix.eureka.*;
+import org.springframework.context.annotation.*;
+import org.springframework.web.client.*;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AdminApplication {
-
+	@Bean
+	@LoadBalanced
+    public RestTemplate getRestTemplate(){
+    	return new RestTemplate();
+	}
 	public static void main(String[] args) {
-		SpringApplication.run(AdminApplication.class, args);
+    	SpringApplication.run(AdminApplication.class, args);
 	}
 
 }
